07084f7c35b1f6d4aa427d16264ebe8cd66c0417
Arjun Praveen Varshney <contact@mythyaverse.com>
2024-09-21 20:08:20 +0530

    Navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import iic from "../assets/iic.png";
+
+const Navbar = () => {
+  const list = ["Home", "Domains", "About Us", "Prizes", "Timeline", "FAQs"];
+  return (
+    <nav className="bg-[#001F2D] flex items-center justify-between px-6 py-3">
+      <img src={logo} alt="logo" />
+      <ul className="flex items-center gap-20">
+        {list.map((item, index) => (
+          <li key={index} className="text-white poppins-semibold">
+            {item}
+          </li>
+        ))}
+      </ul>
+      <img src={iic} alt="iic" />
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Prizes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Prizes = () => {
+  return (
+    <div>
+      <div className="w-4/5 mx-auto bg-secondary py-1 text-[#FAF3DD] font-bold text-[36px] leading-[54px] tracking-[10%] text-center">
+        PRIZES
+      </div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Prizes;
