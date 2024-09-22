a3d2fc79f4c88beef89b3da75a62583ed77eca02
Arjun Praveen Varshney <contact@mythyaverse.com>
2024-09-22 13:55:02 +0530

    added card flip animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const Card = ({ text, bgColor = "bg-secondary" }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsFlipped(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsFlipped(false);
+  };
   return (
     <div
-      className={`rounded-[10px] ${bgColor} text-center px-3 text-white w-1/3 h-60 flex items-center justify-center font-[900] text-[32px] leading-[48px] tracking-[10%]`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="h-60 w-1/3 cursor-pointer relative"
+      style={{ perspective: "1000px" }}
     >
-      {text}
+      <motion.div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "relative",
+          transformStyle: "preserve-3d",
+        }}
+        initial={false}
+        animate={{ rotateY: isFlipped ? 180 : 0 }}
+        transition={{ duration: 0.6, type: "spring" }}
+      >
+        <div
+          className={`rounded-[10px] absolute ${bgColor} text-center px-3 text-white flex items-center justify-center font-[900] text-[32px] leading-[48px] tracking-[10%] h-full w-full`}
+          style={{
+            textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            backgroundOrigin: "border-box",
+            backgroundClip: "padding-box, border-box",
+            backfaceVisibility: "hidden",
+          }}
+        >
+          {text}
+        </div>
+        <div
+          className={`rounded-[10px] absolute ${bgColor} text-center px-3 text-white w-full h-full flex items-center justify-center font-[900] text-[32px] leading-[48px] tracking-[10%]`}
+          style={{
+            textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            backgroundOrigin: "border-box",
+            backgroundClip: "padding-box, border-box",
+            backfaceVisibility: "hidden",
+            transform: "rotateY(180deg)",
+          }}
+        >
+          lorem ipsum dolor sit
+        </div>
+      </motion.div>
     </div>
   );
 };
--- src/components/Prizes.jsx
+++ src/components/Prizes.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-import Card from "./Card";
 import Heading from "./Heading";
+
+const Card = ({ text }) => {
+  return (
+    <div
+      className={`rounded-[10px] bg-secondary text-center px-3 text-white w-1/3 h-60 flex items-center justify-center font-[900] text-[32px] leading-[48px] tracking-[10%]`}
+    >
+      {text}
+    </div>
+  );
+};
 
 const Prizes = () => {
   return (
