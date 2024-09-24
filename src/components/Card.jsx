6af011fb322b6af4f547bfd50a03f722f22875b6
Arjun Praveen Varshney <contact@mythyaverse.com>
2024-09-25 00:30:35 +0530

    almost there!

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ const Card = ({ text, bgColor = "bg-secondary" }) => {
     <div
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className="h-60 w-full md:w-1/3 cursor-pointer relative"
+      className="h-60 w-[90%] mx-auto md:w-1/3 cursor-pointer relative"
       style={{ perspective: "1000px" }}
     >
       <motion.div
