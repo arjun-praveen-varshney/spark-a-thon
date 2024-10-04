5d20acf5f792c2f07e0eb4cff4a15d2fb582c2c1
Arjun Praveen Varshney <arjun.varshney1423@gmail.com>
2024-10-04 18:24:01 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Button = ({ text }) => {
+const Button = ({ text, onClick=()=>{} }) => {
   return (
-    <button className="rounded-[8px] font-medium text-white bg-tertiary py-2 px-8 text-center">
+    <button className="rounded-[8px] font-medium text-white bg-tertiary py-2 px-8 text-center" onClick={onClick}>
       {text}
     </button>
   );
